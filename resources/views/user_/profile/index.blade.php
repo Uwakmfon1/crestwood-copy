e4d5f8df2e07f75f04442108f70e53e6e2aba10d
Sanni David <sannidavidsmart@gmail.com>
2025-01-10 22:49:17 +0100

    feat: fix message

--- resources/views/user_/profile/index.blade.php
+++ resources/views/user_/profile/index.blade.php
@@ -759,6 +759,28 @@
                                                     @csrf
                                                     <input type="hidden" name="screen" value="seven">
                                                     <div class="row">
+                                                        <div>
+                                                        @if(auth()->user()['is_id_approved'] == "pending" || auth()->user()['is_id_approved'] == 'decline') 
+                                                            <div id="" class="alert alert-primary mt-2">
+                                                                <h4 class="text-danger fs-12 fw-bold">
+                                                                @if(!auth()->user()['id_number'] || auth()->user()['is_id_approved'] == 'decline') 
+                                                                    Compliance Notice:
+                                                                @elseif(auth()->user()['id_number'] && auth()->user()['is_id_approved'] == 'pending')
+                                                                    Verification in progress:
+                                                                @endif
+                                                                </h4>
+                                                                <div class="">
+                                                                    <p class="fs-12 text-muted">
+                                                                        @if(!auth()->user()['id_number'] || auth()->user()['is_id_approved'] == 'decline') 
+                                                                            In compliance with applicable laws and Customer Identification Program (CIP) requirements, your information will be securely processed.
+                                                                        @elseif(auth()->user()['id_number'] && auth()->user()['is_id_approved'] == 'pending')
+                                                                            Verification in progress. Please check back later for updates.
+                                                                        @endif
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        @endif
+                                                        </div>
                                                         <div class="col-md-12 my-2">
                                                             <label class="form-label mt-2 text-muted fs-12" for="avatar">ID Type</label>
                                                             <select class="form-control" name="id_type" id="">
@@ -807,22 +829,6 @@
                                                         @if(auth()->user()['is_id_approved'] == 'approved')
 
                                                         @else
-                                                            <div>
-                                                                <div id="" class="alert alert-primary mt-2">
-                                                                    <h4 class="text-danger fs-12 fw-bold">Compliance Notice:</h4>
-                                                                    <div class="">
-                                                                        <p class="fs-12 text-muted">
-                                                                            @if(auth()->user()['id_number'] && auth()->user()['is_id_approved'] == 'pending')
-                                                                                Verification in progress. Please check back later for updates.
-                                                                                <!-- Thank you for submitting your information. Your verification is currently under review, which can take up to 24 hours. We’ll notify you once it’s completed. -->
-                                                                            @elseif(auth()->user()['is_id_approved'] == 'decline' || auth()->user()['id_number'] == null)
-                                                                                In compliance with applicable laws and Customer Identification Program (CIP) requirements, your information will be securely processed.
-                                                                            @endif
-                                                                        </p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        
                                                             <div class="my-2">
                                                                 <button class="btn btn-success">Submit</button>
                                                             </div>
